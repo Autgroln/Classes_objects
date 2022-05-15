f88cf3074810ceeaa12a2a8a4acbbe13f1c0c607
kbaze <kbaze@DESKTOP-MNP1FVE>
2022-05-15 10:14:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Classes_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Luv si", "Kaloyan Bazenikov", 123);
+                /*
+                  book1.title = "Luv si";
+                  book1.author = "Kaloyan Bazenikov";
+                  book1.pages = 124;
+                */
+
+
+
+            Console.WriteLine(book1.author);
+        }
+    }
+}
